78be5d9b6889ee5fc8bbba55e4162d41316a0bf0
Sharmina Akhtar <sakht318@gmail.com>
2018-02-17 13:37:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,10 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(students)
-    @name = students[:name]
-    @grade = students[:grade]
-    @id = students[:id]
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = nil
   end
 
   def self.create_table
@@ -42,7 +42,7 @@ class Student
     end
   end
 
-  def self.create(name:, grade:)
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
